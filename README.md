5da1ae97d85b40ea170960fefe8896b2ead97a6b
PhilomellianOde <piazzajacob23@gmail.com>
2023-10-11 04:44:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,2 @@
+# first-portfolio
+This is my first full-length web development project. The site was written using vanilla HTML, CSS, and JS and is hosted using Github pages.
---
+++ docs/index.js
@@ -0,0 +1,19 @@
+
+
+
+
+function clickFunc(){
+    console.log("Test");
+
+}
+
+function navAboutMe(){
+    document.getElementById("aboutMe-nav").onclick(console.log("test"));
+
+}
+
+const dataButton = 
+
+
+
+
